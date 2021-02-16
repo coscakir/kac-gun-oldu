1f037d7dce8fbb01be7ae27f6ddd2e23c2ac2cca
coskun cakir <csknckr@gmail.com>
2021-02-16 12:10:14 +0000

    fallar duzenlendi

--- pages/api/fal.js
+++ pages/api/fal.js
@@ -1,11 +1,11 @@
 export default (req, res) => {
   const falSonuclari = [
     "- bak burda pikaçu benzeri birşey var görüyor musun, evet ya resmen pikaçu bu... - eee, ne demek yani şimdi bu? - valla, biri seni seçecek heralde, anlamı bu.",
-    "- bir köpek var ama balık gibi kafası. - hani nerde; köpek mi, balık mı? - hmm... köpek balığı.",
-    "- yunus çıktı burda. hmm yunus tatil demek yaaani vizen yazin gelicek 🎉",
-    "- gelecek ayin beşinci gününun şafağında mailine bak. bekledigin haber gelmiş olacak.",
+    "- bir köpek var ama balık gibi kafası. - hani nerde; köpek mi, balık mı? - hmm... köpek balığı. ?!vize?!",
+    "- yunus çıktı burda. hmm yunus tatil demek yaaani vizen yazın gelecekç 🎉",
+    "- gelecek ayin beşinci gününün şafağında mailine bak. bekledigin haber gelmiş olacak.",
     "...bak bak şurda işte... göz var sende vizeyi boşver şimdi. -kız kahve den sonra sana bi kurşun dökelim",
-    "- sana iki vakite kadar bir yol var, ayrica haber alacaksin, elline önemli bir belge geciyor, kismetlerin bol, elline para gececek, evleniyorsun, mutlu bir yuva ve iyi bir i$e sagip oluyorsun...",
+    "- sana iki vakite kadar bir yol var, ayrica haber alacaksin, elline önemli bir belge geciyor, kismetlerin bol, elline para gececek, evleniyorsun, mutlu bir yuva ve iyi bir işe sahip oluyorsun...",
     "- kiz vallahi sende hiç birşey görmüyorum. sende hersey süper, hic merak etme.",
     "- sana iki yol var; biri uzun biri kisa; uzunu hayat yolu, kisasi ayak yolu",
     "- e ne görüyosun? - kahveee!?",
